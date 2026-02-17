314865aeba6adc075872ffa53c9aec8c85ac93c3
fill711 <fiwaeknai@gmail.com>
2026-02-17 22:57:41 +0700

    Initial commit

--- php_api/condb.php
+++ php_api/condb.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE,OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
